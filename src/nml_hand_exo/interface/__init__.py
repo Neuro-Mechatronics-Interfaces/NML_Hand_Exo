530c7b490ebde3909a6cbdfd70fc4f4c04d0c18a
Jonathan Shulgach <jshulgac@andrew.cmu.edu>
2026-04-07 15:25:29 -0400

    fix(ci): make LSL interface imports optional for safety tests

--- src/nml_hand_exo/interface/__init__.py
+++ src/nml_hand_exo/interface/__init__.py
@@ -1,7 +1,15 @@
 from ._interfaces import BaseComm, SerialComm, TCPComm
 from ._hand_exo import HandExo
 from ._fake_hand_exo import FakeHandExo
-from ._lsl_client import LSLClient
-from ._lsl_publisher import LSLMessagePublisher
-from ._lsl_subscriber import LSLMarkerSubscriber, LSLNumericSubscriber
 from ._gesture_controller import GestureController
+
+# LSL dependencies (pylsl/liblsl) are optional in CI and non-streaming installs.
+try:
+	from ._lsl_client import LSLClient
+	from ._lsl_publisher import LSLMessagePublisher
+	from ._lsl_subscriber import LSLMarkerSubscriber, LSLNumericSubscriber
+except Exception:
+	LSLClient = None
+	LSLMessagePublisher = None
+	LSLMarkerSubscriber = None
+	LSLNumericSubscriber = None
